699f21b05321308a3511b94dfb3bf10b54481bbd
AadeshRaut-06 <aadesh_raut@Aadyy.localdomain>
2026-05-25 10:44:42 +0000

    Connected Frontend Backend API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
 function App() {
+
+  const [servers, setServers] = useState([]);
+
+  useEffect(() => {
+    fetchServers();
+  }, []);
+
+  const fetchServers = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/servers");
+      setServers(response.data);
+    } catch (error) {
+      console.error("Error fetching servers", error);
+    }
+  };
+
   return (
-    <div>
+    <div className="container">
       <h1>Cloud Resource Management System</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Server Name</th>
+            <th>IP Address</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {servers.map((server) => (
+            <tr key={server.id}>
+              <td>{server.id}</td>
+              <td>{server.serverName}</td>
+              <td>{server.ipAddress}</td>
+              <td>{server.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
